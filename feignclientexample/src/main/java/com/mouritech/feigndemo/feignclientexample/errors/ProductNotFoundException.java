8e5ee4f6ecc70a9d3ff0b6d787125c2ce28fab74
Prathamesh-Salunkhe <96723431+Prathamesh-Salunkhe@users.noreply.github.com>
2022-02-18 13:28:16 +0530

    Product service

---
+++ feignclientexample/src/main/java/com/mouritech/feigndemo/feignclientexample/errors/ProductNotFoundException.java
@@ -0,0 +1,12 @@
+package com.mouritech.feigndemo.feignclientexample.errors;
+public class ProductNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ProductNotFoundException(String msg) {
+			super(msg);
+	}
+}
